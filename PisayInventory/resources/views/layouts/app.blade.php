84607d1e0b21995e62d1171caeacff7265b25ae6
Charles Dave Alboroto <davecharles770@gmail.com>
2025-02-07 11:05:51 +0800

    Added Icon Beside the Title

--- PisayInventory/resources/views/layouts/app.blade.php
+++ PisayInventory/resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>@yield('title', 'PSHS Inventory')</title>
+        <link rel="icon" href="{{ asset('pisaylogo.png') }}" type="image/x-icon">
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <!-- Bootstrap Icons -->
