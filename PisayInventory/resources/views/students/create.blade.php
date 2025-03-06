08e6a7ea10bb3920e2647d24c85c4c6948ea5792
Kent Tac-an <tacankent315@gmail.com>
2025-03-06 16:41:33 +0800

    Add 'Back to List' button to the create.blade.php

--- PisayInventory/resources/views/students/create.blade.php
+++ PisayInventory/resources/views/students/create.blade.php
@@ -4,13 +4,16 @@
 
 @section('content')
 <div class="container-fluid px-4">
+    <div class="d-flex justify-content-between align-items-center mb-4">
+        <h2 class="mb-0">
+            <i class="fas fa-user-plus me-1"></i>
+            Add New Student
+        </h2>
+        <a href="{{ route('students.index') }}" class="btn btn-secondary">
+            <i class="bi bi-arrow-left"></i> Back to List
+        </a>
+    </div>
     <div class="card mb-4">
-        <div class="card-header">
-            <h4 class="mb-0">
-                <i class="fas fa-user-plus me-1"></i>
-                Add New Student
-            </h4>
-        </div>
         <div class="card-body">
             <form action="{{ route('students.store') }}" method="POST">
                 @csrf
